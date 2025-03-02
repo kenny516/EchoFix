3cd86ec2542b358429af691b8c4d9768c5ac2553
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-03-02 22:34:13 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using EchoFix.services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IAudioProcessingService, AudioProcessingService>();
+builder.Services.AddScoped<IAudioFileUtils, AudioFileUtils>();
 
 var app = builder.Build();
 
@@ -22,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=AudioProcessor}/{action=Index}/{id?}");
 
 app.Run();
